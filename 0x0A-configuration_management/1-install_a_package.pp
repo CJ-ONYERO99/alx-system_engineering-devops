d7652e4e989eb2500feda278d36afd7fcfb8004c
CJ-ONYERO99 <chimeziejoshuaonyero999@gmail.com>
2024-04-24 16:53:28 -0700

    Task Done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,12 @@
-package { 'python3-pip':
-  ensure => installed,
+class flask_install {
+  package { 'python3-pip':
+    ensure => installed,
+  }
+
+  exec { 'install_flask':
+    command => '/usr/bin/pip3 install flask==2.1.0',
+    require => Package['python3-pip'],
+  }
 }
 
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
-  require => Package['python3-pip'],
-}
+include flask_install
