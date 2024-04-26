180711aea0163eb9ce2e49a28b9ce1ba9a1bedf6
CJ-ONYERO99 <67972505+CJ-ONYERO99@users.noreply.github.com>
2024-04-27 00:24:34 +0100

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,16 @@
-# Seting up my client config file
+# Puppet script to configure SSH client settings
+include stdlib
+
 file_line { 'Turn off passwd auth':
-  path   => '/etc/ssh/sshd_config',
-  line   => 'PasswordAuthentication no',
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  replace => true,
 }
 
-file_line { 'Declare identity file':
+file_line { 'Delare identity file':
+  ensure => present,
   path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/school',
-  match  => '^#?IdentityFile.*',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
