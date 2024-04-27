928e0b36be6855ced5751639fe9afa25c43f4970
CJ-ONYERO99 <67972505+CJ-ONYERO99@users.noreply.github.com>
2024-04-27 11:15:45 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 # Install a flask package
 
-package {'Flask':
+package { 'python3-pip':
+  ensure => installed,
+}
+
+package { 'python3-flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
