4a8cb74e3f4901ab817207257ef8926141c706d2
CJ-ONYERO99 <67972505+CJ-ONYERO99@users.noreply.github.com>
2024-04-27 12:03:52 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,17 +1,28 @@
 # Install a flask package
 
+# Install Python 3 pip
 package { 'python3-pip':
   ensure => installed,
 }
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# Install Flask and Werkzeug with specified versions
+exec { 'install_flask_and_werkzeug':
+  command => '/usr/bin/pip3 install -q Flask==2.1.0 Werkzeug==2.0.2',
+  path    => ['/usr/bin'],
+  require => Package['python3-pip'],
 }
 
-exec { 'update_flask_version':
-  command     => '/usr/bin/pip3 install -q --upgrade Flask==2.1.0',
+# Add /usr/local/bin to PATH
+file_line { 'add_flask_path':
+  ensure => present,
+  path   => '/etc/environment',
+  line   => 'PATH="$PATH:/usr/local/bin"',
+  notify => Exec['reload_environment'],
+}
+
+# Reload environment variables
+exec { 'reload_environment':
+  command     => 'source /etc/environment',
   path        => ['/usr/bin'],
   refreshonly => true,
-  subscribe   => Package['flask'],
 }
