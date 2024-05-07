f0eb1993ed6ae1cc2b9b26d26427e39cdf79ba13
CJ-ONYERO99 <67972505+CJ-ONYERO99@users.noreply.github.com>
2024-05-07 18:25:24 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,15 +1,6 @@
-# Install a flask package
-
-exec { 'update':
-  command => '/usr/bin/apt-get update'
-}
-
-package { 'python3-pip':
-  ensure => installed,
-  require => Exec['update'],
-}
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  require => Package['python3-pip'],
+# A puppet manifest installing flask v2.1.0 which is a package from pip3.
+# Am also specifying the version of flask to install using the ensure attribute
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => pip3,
 }
